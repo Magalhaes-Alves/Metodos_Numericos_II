3111de0d330066feed734914fe72fb4aabd7b31d
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-05-13 21:47:06 -0300

    Menu bonitinho

--- tarefa5/Main.py
+++ tarefa5/Main.py
@@ -1,15 +1,61 @@
-a1 = -0.861136311
-a2 = -0.339981043
-a3 = 0.339981043
-a4 = 0.861136311
+import integracoes
 
-a= [a1,a2,a3,a4]
+print("\nCalculadora da Integral da função f(x)=(sin(2*x) + 4*(x**2) + 3*x)**2 pelas fórmulas de Newton-Cotes")
 
-valor = 1
+print("------------------------Menu---------------------------")
+print("0-Calcular a Integral da função com quadratura de Gauss Lagrende com 2 pontos")
+print("1-Calcular a Integral da função com quadratura de Gauss Lagrende com 3 pontos")
+print("2-Calcular a Integral da função com quadratura de Gauss Lagrende com 4 pontos")
+print("-------------------------------------------------------\n")
+opcao = input(("Digite a opção que você deseja:"))
 
-for i in range(4):
-    if(i!=1):
-        valor= valor* (a[1] - a[i])
-        print(str(valor) + "  " + str(i))
 
-print(valor)
+if opcao == '0':
+    limiteInferior = float(input("\nDigite o limite inferior da integral:"))
+    
+    limiteSuperior = float(input("Digite o limite superior da integral:"))
+
+    grauErro = int(input("Digite o  grau do erro:"))
+
+    erro = 1/(10**grauErro)
+
+
+    print(f"Calculando a integral da função f(x)=f(x)=(sin(2*x) + 4*(x**2) + 3*x)**2 de {limiteInferior} a {limiteSuperior}\n")
+
+    print(f"\n-Quadratura de Gauss Lagrende com 2 pontos com erro {erro}")
+
+    integracoes.gaussLegrende2Pontos(limiteInferior,limiteSuperior,erro)
+
+elif opcao == '1':
+    limiteInferior = float(input("\nDigite o limite inferior da integral:"))
+    
+    limiteSuperior = float(input("Digite o limite superior da integral:"))
+
+    grauErro = int(input("Digite o  grau do erro:"))
+
+    erro = 1/(10**grauErro)
+
+
+    print(f"Calculando a integral da função f(x)=f(x)=(sin(2*x) + 4*(x**2) + 3*x)**2 de {limiteInferior} a {limiteSuperior}\n")
+
+    print(f"\n-Quadratura de Gauss Lagrende com 3 pontos com erro {erro}")
+
+    integracoes.gaussLegrende3Pontos(limiteInferior,limiteSuperior,erro)
+
+elif opcao=='2':
+    limiteInferior = float(input("\nDigite o limite inferior da integral:"))
+    
+    limiteSuperior = float(input("Digite o limite superior da integral:"))
+
+    grauErro = int(input("Digite o  grau do erro:"))
+
+    erro = 1/(10**grauErro)
+
+
+    print(f"Calculando a integral da função f(x)=f(x)=(sin(2*x) + 4*(x**2) + 3*x)**2 de {limiteInferior} a {limiteSuperior}\n")
+
+    print(f"\n-Quadratura de Gauss Lagrende com 4 pontos com erro {erro}")
+
+    integracoes.gaussLegrende4Pontos(limiteInferior,limiteSuperior,erro)
+else:
+    print("Você digitou uma opção inválida.")
